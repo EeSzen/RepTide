8d70ddd0e4751827750d977dead7c11aaee91561
Ee Szen <eeszenlee@gmail.com>
2025-09-27 22:18:25 +0800

    Non Functional Edit Fragments and ViewModel for Exercise and Workout

---
+++ app/src/main/java/com/eeszen/reptide/ui/manageExercise/EditExerciseViewModel.kt
@@ -0,0 +1,7 @@
+package com.eeszen.reptide.ui.manageExercise
+
+import androidx.lifecycle.ViewModel
+
+class EditExerciseViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/eeszen/reptide/ui/manageWorkout/EditWorkoutViewModel.kt
@@ -0,0 +1,7 @@
+package com.eeszen.reptide.ui.manageWorkout
+
+import androidx.lifecycle.ViewModel
+
+class EditWorkoutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
