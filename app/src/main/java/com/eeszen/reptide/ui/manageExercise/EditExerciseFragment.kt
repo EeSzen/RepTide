a8b67fdf4a02f9487880c4b61a60b8d0f2cacc48
Ee Szen <eeszenlee@gmail.com>
2025-10-05 03:59:06 +0800

    Main exercise viewModel and Fragment to edit exercise and also to show the update page and the chosen exercise values

--- app/src/main/java/com/eeszen/reptide/ui/manageExercise/EditExerciseFragment.kt
+++ app/src/main/java/com/eeszen/reptide/ui/manageExercise/EditExerciseFragment.kt
@@ -1,31 +1,93 @@
-//package com.eeszen.reptide.ui.manageExercise
-//
-//import androidx.fragment.app.viewModels
-//import android.os.Bundle
-//import androidx.fragment.app.Fragment
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import com.eeszen.reptide.R
-//
-//class EditExerciseFragment : Fragment() {
-//
-//    companion object {
-//        fun newInstance() = EditExerciseFragment()
-//    }
-//
-//    private val viewModel: EditExerciseViewModel by viewModels()
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        // TODO: Use the ViewModel
-//    }
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        return inflater.inflate(R.layout.fragment_edit_exercise, container, false)
-//    }
-//}
+package com.eeszen.reptide.ui.manageExercise
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.eeszen.reptide.R
+import com.eeszen.reptide.data.model.WorkoutType
+import kotlinx.coroutines.launch
+
+class EditExerciseFragment : BaseManageExerciseFragment() {
+    private val viewModel: EditExerciseViewModel by viewModels()
+    private val args: EditExerciseFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupExerciseCategory()
+
+        viewModel.loadExercise(args.exerciseId)
+
+        lifecycleScope.launch {
+            viewModel.exercise.collect { exercise ->
+                exercise?.let {
+                    binding.run {
+                        toolbarTitle.text = getString(R.string.update_exercise,it.name)
+                        toolbar.setNavigationOnClickListener {
+                            findNavController().popBackStack()
+                        }
+
+                        etName.setText(it.name)
+
+                        // NumberPickers
+                        npSets.minValue = 1
+                        npSets.maxValue = 10
+                        npSets.wrapSelectorWheel = true
+                        npSets.value = it.sets
+
+                        npReps.minValue = 1
+                        npReps.maxValue = 30
+                        npReps.wrapSelectorWheel = true
+                        npReps.value = it.reps
+
+                        // Category Chip
+                        for (i in 0 until chipGroup.childCount) {
+                            val chip = binding.chipGroup.getChildAt(i) as com.google.android.material.chip.Chip
+                            chip.isChecked = (chip.tag == it.category)
+                        }
+                    }
+                }
+            }
+        }
+
+        binding.mbSubmit.setOnClickListener {
+            binding.run {
+                val name = etName.text.toString()
+                val sets = npSets.value
+                val reps = npReps.value
+
+                // find the checked chip
+                val checkedChipId = chipGroup.checkedChipId
+                val checkedChip = chipGroup.findViewById<com.google.android.material.chip.Chip>(checkedChipId)
+
+                val category = checkedChip?.tag as? WorkoutType
+
+                if (category != null) {
+                    viewModel.editExercise(name, sets, reps, category)
+                } else {
+                    showError("Please select a category")
+                }
+            }
+        }
+
+        // observe finish to go back when saved
+        lifecycleScope.launch {
+            viewModel.finish.collect {
+                findNavController().popBackStack()
+            }
+        }
+
+        // observe error
+        lifecycleScope.launch {
+            viewModel.error.collect{ errorMessage ->
+                showError(errorMessage)
+            }
+        }
+    }
+}
--- app/src/main/java/com/eeszen/reptide/ui/manageExercise/EditExerciseViewModel.kt
+++ app/src/main/java/com/eeszen/reptide/ui/manageExercise/EditExerciseViewModel.kt
@@ -1,7 +1,67 @@
 package com.eeszen.reptide.ui.manageExercise
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eeszen.reptide.data.model.Exercise
+import com.eeszen.reptide.data.model.WorkoutType
+import com.eeszen.reptide.data.repo.ExerciseRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class EditExerciseViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class EditExerciseViewModel(
+    private val repo: ExerciseRepo = ExerciseRepo.getInstance()
+) : ViewModel() {
+
+    private val _exercise = MutableStateFlow<Exercise?>(null)
+    val exercise: StateFlow<Exercise?> = _exercise
+
+    private val _finish = MutableSharedFlow<Unit>()
+    val finish: SharedFlow<Unit> = _finish
+
+    private val _error = MutableSharedFlow<String>()
+    val error: SharedFlow<String> = _error
+
+    fun loadExercise(id:Int){
+        val clickedExercise = repo.getExercise(id)
+        if (clickedExercise != null){
+            _exercise.value = clickedExercise
+        }else{
+            viewModelScope.launch {
+                _error.emit("Exercise with ID: $id is not found")
+            }
+        }
+    }
+
+    fun editExercise(name: String, sets: Int, reps: Int, category: WorkoutType) {
+        try {
+            require(name.isNotBlank()) { "Name cannot be blank" }
+            require(category.toString().isNotBlank()) { "Category cannot be blank" }
+
+            val oldExercise = _exercise.value
+            if (oldExercise != null) {
+                val updated = oldExercise.copy(
+                    name = name,
+                    category = category,
+                    sets = sets,
+                    reps = reps
+                )
+
+                viewModelScope.launch {
+                    repo.updateExercise(updated)
+                    _finish.emit(Unit)
+                }
+            } else {
+                viewModelScope.launch {
+                    _error.emit("No exercise loaded for editing")
+                }
+            }
+        } catch (e: Exception) {
+            viewModelScope.launch { _error.emit(e.message.toString()) }
+        }
+    }
+
 }
