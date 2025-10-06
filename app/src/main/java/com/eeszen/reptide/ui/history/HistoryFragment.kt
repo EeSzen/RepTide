4c757998f2e7e8f5fc0e2002c0c41ba7122a486c
Ee Szen <eeszenlee@gmail.com>
2025-10-06 09:57:48 +0800

    Base of the History Fragment that shows the recyclerView now

---
+++ app/src/main/java/com/eeszen/reptide/ui/history/HistoryFragment.kt
@@ -0,0 +1,40 @@
+package com.eeszen.reptide.ui.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.eeszen.reptide.databinding.FragmentHistoryBinding
+import com.eeszen.reptide.ui.adapter.HistoryAdapter
+
+class HistoryFragment : Fragment() {
+    private lateinit var binding: FragmentHistoryBinding
+    private lateinit var historyAdapter: HistoryAdapter
+    private val viewModel: HistoryViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val completedLogs = viewModel.getCompletedWorkouts()
+
+        historyAdapter = HistoryAdapter(completedLogs) { workout ->
+            // TODO: navigate to workout details later
+        }
+
+        binding.rvHistory.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = historyAdapter
+        }
+    }
+}
