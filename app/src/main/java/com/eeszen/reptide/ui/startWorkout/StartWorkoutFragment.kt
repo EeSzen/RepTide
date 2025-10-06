efd3dbf4e7a2126e87f87817ef87b39d3a257174
Ee Szen <eeszenlee@gmail.com>
2025-10-06 09:51:18 +0800

    Updated the adapter to change the workoutLog's exercises field as the updatedExercise and added the button setOnClickListener that checks for all exercises inside the workoutLog and updates the workoutLog after all exercises are done

--- app/src/main/java/com/eeszen/reptide/ui/startWorkout/StartWorkoutFragment.kt
+++ app/src/main/java/com/eeszen/reptide/ui/startWorkout/StartWorkoutFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -19,6 +20,7 @@ import com.eeszen.reptide.data.repo.WorkoutRepo
 import com.eeszen.reptide.databinding.FragmentStartWorkoutBinding
 import com.eeszen.reptide.ui.adapter.StartExerciseAdapter
 import com.google.android.material.chip.Chip
+import com.google.android.material.snackbar.Snackbar
 
 class StartWorkoutFragment : Fragment() {
     private val args: StartWorkoutFragmentArgs by navArgs()
@@ -49,12 +51,39 @@ class StartWorkoutFragment : Fragment() {
             }
 
 //            // Setup ViewPager with adapter
-            startExerciseAdapter = StartExerciseAdapter(workoutLog.exercises)
+//            startExerciseAdapter = StartExerciseAdapter(workoutLog.exercises)
+            startExerciseAdapter = StartExerciseAdapter(workoutLog.exercises) { updatedExercise, index ->
+                workoutLog.exercises[index] = updatedExercise
+            }
+            binding.viewPagerExercises.adapter = startExerciseAdapter
+
             viewPagerExercises.adapter = startExerciseAdapter
 
             setupChipNavigation(workout)
             syncChipsWithPager(workout)
+
+            mbFinishWorkout.setOnClickListener {
+                viewModel.currentWorkoutLog = workoutLog
+                val allExercisesCompleted = workoutLog.exercises.all { it.isCompleted }
+
+                if (allExercisesCompleted) {
+                    workoutLog.isCompleted = true
+                    workoutLog.finishedAt = System.currentTimeMillis()
+                    WorkoutRepo.getInstance().addWorkoutLog(workoutLog)
+
+                    val action = StartWorkoutFragmentDirections.actionStartWorkoutFragmentToHistoryFragment()
+                    findNavController().navigate(action)
+                } else {
+                    showError("Finish all exercises first!")
+                }
+            }
         }
+    }
+
+    fun showError(msg:String){
+        val snackbar = Snackbar.make(binding.root,msg, Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.red))
+        snackbar.show()
     }
 
     // Mapper Function (convert Exercise to ExerciseLog)
@@ -79,7 +108,7 @@ class StartWorkoutFragment : Fragment() {
             workoutId = id,
             name = name,
             type = type,
-            exercises = exercises.map { it.toExerciseLog() }
+            exercises = exercises.map { it.toExerciseLog() }.toMutableList()
         )
     }
 
--- app/src/main/java/com/eeszen/reptide/ui/startWorkout/StartWorkoutViewModel.kt
+++ app/src/main/java/com/eeszen/reptide/ui/startWorkout/StartWorkoutViewModel.kt
@@ -2,6 +2,7 @@ package com.eeszen.reptide.ui.startWorkout
 
 import androidx.lifecycle.ViewModel
 import com.eeszen.reptide.data.model.Workout
+import com.eeszen.reptide.data.model.logs.WorkoutLog
 import com.eeszen.reptide.data.repo.WorkoutRepo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,8 +14,8 @@ class StartWorkoutViewModel(
     private val _workouts = MutableStateFlow<List<Workout>>(emptyList())
     val workouts: StateFlow<List<Workout>> = _workouts
 
-    fun getWorkoutById(id:Int) : Workout? {
-        return repo.getWorkout(id)
-    }
+    var currentWorkoutLog: WorkoutLog? = null
+
+    fun getWorkoutById(id:Int) : Workout? = repo.getWorkout(id)
 
 }
