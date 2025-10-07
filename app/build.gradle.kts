ba1f1d35194d1ef7d2b1ae249d2370ede7722be3
Ee Szen <eeszenlee@gmail.com>
2025-10-07 15:47:38 +0800

    Added dependencies for MPAndroidCharts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.7.2")
     implementation("androidx.room:room-ktx:2.7.2")
     kapt("androidx.room:room-compiler:2.7.2")
+
+    // Chart Dependency
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
